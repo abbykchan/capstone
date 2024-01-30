1da65f882967d1f5dfe0b2c765db4963913b803c
jaydenhs <j3hsiao@uwaterloo.ca>
2024-01-30 14:25:47 -0500

    Initialize thermostat UI

---
+++ thermometer-ui/app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, request, jsonify
+
+app = Flask(__name__)
+
+# Initial desired temperature
+desired_temperature = 20.0
+
+@app.route('/')
+def home():
+    return render_template('index.html', desired_temperature=desired_temperature)
+
+@app.route('/get_desired_temperature', methods=['GET'])
+def get_desired_temperature():
+    return jsonify({'desired_temperature': desired_temperature})
+
+@app.route('/set_desired_temperature', methods=['POST'])
+def set_desired_temperature():
+    global desired_temperature
+    new_temperature = float(request.form.get('new_temperature'))
+    desired_temperature = new_temperature
+    return jsonify({'success': True, 'desired_temperature': desired_temperature})
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=8080)
